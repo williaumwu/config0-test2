e4ae87bf940f4b3552f441ca25673bea6df6cae0
gary <gary>
2025-04-12 18:46:59 +0800

    'update'

---
+++ execgroups/_config0_configs/natgw_vpc/_actions/30-exec_terraform.py
@@ -0,0 +1,10 @@
+def default():
+    return {
+        'method': 'shelloutconfig',
+        'metadata': {
+            'env_vars': [],
+            'shelloutconfigs': ['williaumwu:::config0-test1::resource_wrapper']
+        }
+    }
+
+
---
+++ execgroups/_config0_configs/natgw_vpc/_chrootfiles/var/tmp/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_default_region" {
+  default = "eu-west-1"
+}
+
+variable "public_subnet_id" {}
+variable "private_route_table_id" {}
+variable "nat_gateway_name" {}
+
+variable "cloud_tags" {
+  description = "additional tags as a map"
+  type        = map(string)
+  default     = {}
+}
+
